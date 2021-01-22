8f7a7cf9fdaa8d3e5c4849c45810e267ffe34e27
Glitch (secocobackend) <none>
2021-01-22 02:39:06 +0000

    Actualización de Zonas

--- email.js
+++ email.js
@@ -21,7 +21,6 @@ module.exports = class email {
       subject: "",
       text: ""
     };
-
   }
   async enviarCorreo(destinatario, asunto, informacion) {
     if (asunto == "SeCoCo - Citación para Prueba COVID-19") {
@@ -34,17 +33,32 @@ module.exports = class email {
         informacion.E.charAt(4) == 1 ? "Disminución de Olfato y Sabor" : "";
 
       mailOptions.text = `Estimado Ciudadano\n\nTeniendo en cuenta que usted ${informacion.N} identificado con el documento ${informacion.I} ha presentado síntomas de ${DF} ${FB} ${FT} ${CS} ${TS} ${DOS}; ha sido citado para una prueba Obligatoria de COVID-19, la cual se realizará en el transcurso del día: ${informacion.F} en su domicilio (${informacion.D}).\n\nRecuerde mantenerse en cuarentena preventiva hasta nuevo aviso.\n\nGracias por su Atención\n\nP.D: La cura viene en camino =D`;
-    }else if (asunto  == "SeCoCo - Aislamiento de Zona"){
-      mailOptions.text =`Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto poner la localidad en cuarentena.\n\nGracias por su atención.`;
-    }else if(asunto  == "SeCoCo - Levantamiento de Aislamiento de Zona"){
-      mailOptions.text =`Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto levantar la cuarentena de la localidad.\n\nGracias por su atención.`
-    }else if(asunto  == "SeCoCo - Continua Aislamiento de Zona"){
-      mailOptions.text =`Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto continuar con la cuarentena de la localidad.\n\nGracias por su atención.`
-    }else if(asunto  == "SeCoCo - Continua Sin Aislamiento la Zona"){
-      mailOptions.text =`Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto continuar sin la cuarentena en la localidad.\n\nGracias por su atención.`
-    }else {
+    } else if (asunto == "SeCoCo - Aislamiento de Zona") {
+      mailOptions.text = `Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto poner la localidad en cuarentena.\n\nGracias por su atención.`;
+    } else if (asunto == "SeCoCo - Levantamiento de Aislamiento de Zona") {
+      mailOptions.text = `Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto levantar la cuarentena de la localidad.\n\nGracias por su atención.`;
+    } else if (asunto == "SeCoCo - Continua Aislamiento de Zona") {
+      mailOptions.text = `Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto continuar con la cuarentena de la localidad.\n\nGracias por su atención.`;
+    } else if (asunto == "SeCoCo - Continua Sin Aislamiento la Zona") {
+      mailOptions.text = `Estimado Ciudadano\n\nTeniendo en cuenta que en la localidad donde reside (${informacion.ZN}) tiene ${informacion.PA}% de infectados con COVID-19. El distrito de Bogotá D.C ha dispuesto continuar sin la cuarentena en la localidad.\n\nGracias por su atención.`;
+    } else if (asunto == "SeCoCo - Actualización de resultado de examen") {
+      let dato =
+        informacion == "A"
+          ? "Activo"
+          : informacion == "I"
+          ? "Inactivo"
+          : informacion == "S"
+          ? "Solicitado"
+          : informacion == "P"
+          ? "Pendiente"
+          : "Examen no Tomado";
       mailOptions.text =
-        "Revisa tus sintomas, tuviste contacto en los ultimos dias con una persona con COVID";
+        "Estimado Ciudadano\n\nEl resultado de su examen fue actualizado a: " +
+        dato +
+        "\n\nGracias por su atención.";
+    } else {
+      mailOptions.text =
+        "Estimado Ciudadano\n\nRevise sus sintomas, estuvo en contacto en los ultimos días con una persona activa con COVID-19\n\nGracias por su atención.";
     }
     mailOptions.to = destinatario;
     mailOptions.subject = asunto;
